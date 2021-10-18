3d4085e17a5a505f654ca4c89829c4e5961872c3
nguyen-h-jp <nguyenphuhung02@gmail.com>
2021-10-18 22:46:34 +0900

    optimize extra assignment_1_1

--- SQL/2. Extra Assignment/Extra Assignment 1_1.sql
+++ SQL/2. Extra Assignment/Extra Assignment 1_1.sql
@@ -1,17 +1,18 @@
 -- Exercise 1
+DROP DATABASE IF EXISTS fresher_training_management;
 CREATE DATABASE fresher_training_management;
 use fresher_training_management;
+ALTER DATABASE testing_system CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE trainee (
-    TraineeID 		INT NOT NULL AUTO_INCREMENT,
-    Full_Name 		VARCHAR(50),
-    Birth_Date 		DATE,
-    Gender 			ENUM('male', 'female', 'unknown'),
-    ET_IQ 			INT CHECK(0 <= ET_IQ AND ET_IQ <= 20),
-    ET_Gmath 		INT CHECK(0 <= ET_Gmath AND ET_Gmath <= 20),
-    ET_English 		INT CHECK(0 <= ET_English AND ET_English <= 20),
-    Training_Class 	VARCHAR(50),
-    Evalution_Notes VARCHAR(500),
-    UNIQUE KEY(TraineeID)
+    trainee_id 		INT NOT NULL AUTO_INCREMENT UNIQUE,
+    full_name		VARCHAR(50) CHAR SET utf8mb4 NOT NULL,
+    birth_date 		DATE NOT NULL,
+    gender 			ENUM('male', 'female', 'unknown'),
+    et_iq 			INT CHECK(0 <= ET_IQ AND ET_IQ <= 20),
+    et_gmath 		INT CHECK(0 <= ET_Gmath AND ET_Gmath <= 20),
+    et_english 		INT CHECK(0 <= ET_English AND ET_English <= 20),
+    training_class 	VARCHAR(50) CHAR SET utf8mb4 NOT NULL,
+    evalution_notes VARCHAR(500) CHAR SET utf8mb4 NOT NULL
 );
 
-ALTER TABLE trainee ADD VTI_Acount VARCHAR(50) NOT NULL UNIQUE;
+ALTER TABLE trainee ADD vti_account VARCHAR(50) CHAR SET utf8mb4 NOT NULL UNIQUE;
