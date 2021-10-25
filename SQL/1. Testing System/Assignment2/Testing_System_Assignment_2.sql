17c1f24d959ae78b9c003882e9e66118c7f1fe12
nguyen-h-jp <nguyenphuhung02@gmail.com>
2021-10-25 20:25:17 +0900

    resummit assignment 2

--- SQL/1. Testing System/Assignment2/Testing_System_Assignment_2.sql
+++ SQL/1. Testing System/Assignment2/Testing_System_Assignment_2.sql
@@ -2,13 +2,17 @@ DROP DATABASE IF EXISTS testing_system;
 CREATE DATABASE testing_system;
 USE testing_system;
 ALTER DATABASE testing_system CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- table 1
+
 DROP TABLE IF EXISTS department; 
 CREATE TABLE department(
     department_id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(50) CHAR SET UTF8MB4 UNIQUE NOT NULL
 );
+
 -- thêm data vào bảng department
+
 INSERT INTO department(department_name)
 VALUES
 	('Marketing'	),
@@ -21,20 +25,26 @@ VALUES
 	('Giám đốc'		),
 	('Thư kí'		),
 	('Bán hàng'		);
+    
 -- table 2 
+
 DROP TABLE IF EXISTS `position`;
 CREATE TABLE `position`(
     position_id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     position_name 	ENUM('Dev','Test','Scrum Master','PM') 
 );
+
 -- thêm data vào bảng position
+
 INSERT INTO `position`(position_name)
 VALUES 
 	('Dev'),
     ('Test'),
     ('Scrum Master'),
     ('PM');
+    
 -- table 3
+
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account`(
     account_id 		INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -47,7 +57,9 @@ CREATE TABLE `account`(
     FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE,
     FOREIGN KEY (position_id) REFERENCES `position`(position_id) ON DELETE CASCADE
 );
+
 -- thêm data vào bảng account
+
 INSERT INTO `account`(email, username, full_name, department_id, position_id, create_date)
 VALUES
 	('a@gmail.com', 'a1', 'Nguyễn Văn A', 1, 1, '2021/09/18'),
@@ -61,7 +73,9 @@ VALUES
     ('h@gmail.com', 'h1', 'Nguyễn Văn H', 3, 3, '2021/06/16'),
     ('i@gmail.com', 'i1', 'Nguyễn Văn I', 3, 3, '2021/06/16'),
     ('k@gmail.com', 'k1', 'Nguyễn Văn K', 3, 3, '2021/06/16');
+    
 -- table 4
+
 DROP TABLE IF EXISTS `group`;
 CREATE TABLE `group`(
     group_id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -70,7 +84,9 @@ CREATE TABLE `group`(
     create_date DATE,
     FOREIGN KEY (creator_id) REFERENCES `account`(account_id) ON DELETE CASCADE
 );
+
 -- thêm data vào bảng group
+
 INSERT INTO `group`(group_name, creator_id, create_date)
 VALUES
 	('group1', 2, '2017/10/18'),
@@ -81,7 +97,9 @@ VALUES
     ('group6', 1, '2021/10/15'),
     ('group7', 5, '2021/10/16'),
     ('group8', 4, '2021/10/15');
+    
 -- table 5
+
 DROP TABLE IF EXISTS group_account;
 CREATE TABLE group_account(
     group_id 	INT NOT NULL, -- QUAN HE NHIEU NHIEU 
@@ -91,7 +109,9 @@ CREATE TABLE group_account(
     FOREIGN KEY (group_id) REFERENCES `group`(group_id) ON DELETE CASCADE,
     FOREIGN KEY (account_id) REFERENCES `account`(account_id) ON DELETE CASCADE
 );
+
 -- thêm data vào bảng group_account
+
 INSERT INTO group_account(group_id, account_id, join_date)
 VALUES
 	(1, 1, '2021/10/18'),
@@ -110,24 +130,32 @@ VALUES
     (5, 5, '2021/10/16'),
     (6, 1, '2021/10/15'),
     (6, 2, '2021/10/15');
+    
 -- table 6
+
 DROP TABLE IF EXISTS type_question;
 CREATE TABLE type_question(
     type_id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_name 	ENUM('essay','multiple_choice')
 );
+
 -- thêm data vào bảng type_question
+
 INSERT INTO type_question(type_name)
 VALUES
 	('multiple_choice'),
 	('essay');
+    
 -- table 7
+
 DROP TABLE IF EXISTS category_question;
 CREATE TABLE category_question(
     category_id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     category_name 	VARCHAR(50) CHAR SET UTF8MB4 NOT NULL UNIQUE
 );
+
 -- thêm data vào bảng category_question
+
 INSERT INTO category_question(category_name)
 VALUES
 	('Java'),
@@ -136,7 +164,9 @@ VALUES
 	('Postman'),
 	('Ruby'),
 	('Javascript');
+    
 -- table 8
+
 DROP TABLE IF EXISTS question;
 CREATE TABLE question(
     question_id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -149,7 +179,9 @@ CREATE TABLE question(
     FOREIGN KEY (type_id) REFERENCES type_question(type_id) ON DELETE CASCADE,
     FOREIGN KEY (creator_id) REFERENCES `account`(account_id) ON DELETE CASCADE
 );
+
 -- thêm data vào bảng question
+
 INSERT INTO question(content, category_id, type_id, creator_id, create_date)
 VALUES
 	('Java là gì'			, 1, 1, 1, '2021/10/18'),
@@ -159,6 +191,7 @@ VALUES
 	('Postman là gì'		, 4, 2, 2, '2021/10/18'),
     ('Ruby là gì'			, 5, 2, 2, '2021/10/18');
 -- table 9
+
 DROP TABLE IF EXISTS answer;
 CREATE TABLE answer(
     answer_id 		INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -167,7 +200,9 @@ CREATE TABLE answer(
     is_correct 		BOOLEAN NOT NULL,
     FOREIGN KEY (question_id) REFERENCES question(question_id) ON DELETE CASCADE
 );
+
 -- thêm data vào bảng answer
+
 INSERT INTO answer(content, question_id, is_correct)
 VALUES
 	('Java là ngôn ngữ lập trình', 1, true),
@@ -184,7 +219,9 @@ VALUES
 	('Postman là tên một món ăn', 5, false);
 	-- ('Ruby là ngôn ngữ lập trình kịch bản', 6, true),
 	-- ('Ruby là một trò trơi', 6, false);
+    
 -- table 10
+
 DROP TABLE IF EXISTS exam;
 CREATE TABLE exam(
     exam_id 		INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -197,7 +234,9 @@ CREATE TABLE exam(
     FOREIGN KEY (category_id) REFERENCES category_question(category_id) ON DELETE CASCADE,
     FOREIGN KEY (creator_id) REFERENCES `account`(account_id) ON DELETE CASCADE
 );
+
 -- thêm data vào bảng exam
+
 INSERT INTO exam(`code`, title, category_id, duration, creator_id, create_date)
 VALUES
 	('ABC1', 'title 1', 1, 15, 5, '2021/10/18'),
@@ -206,8 +245,9 @@ VALUES
 	('ACD3', 'title 3', 4, 90, 4, '2019/12/19'),
 	('ACB4', 'title 4', 5, 60, 2, '2021/10/18'),
 	('EFD5', 'title 5', 2, 75, 1, '2021/10/18');
-SELECT * FROM exam;
+
 -- table 11
+
 DROP TABLE IF EXISTS exam_question;
 CREATE TABLE exam_question(
     exam_id 		INT NOT NULL,
@@ -216,6 +256,7 @@ CREATE TABLE exam_question(
     FOREIGN KEY (exam_id) REFERENCES exam(exam_id) ON DELETE CASCADE,
     FOREIGN KEY (question_id) REFERENCES question(question_id) ON DELETE CASCADE
 );
+
 INSERT INTO exam_question(exam_id, question_id)
 VALUES
 	(1, 2),
@@ -229,5 +270,3 @@ VALUES
 	(5, 1),
 	(5, 4),
     (5, 5);
-SELECT *
-FROM exam_question;
